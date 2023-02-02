14e23711b76207dd57cd655821b53d45b3e360d2
Corey <reg@pathrider.ca>
2023-02-02 12:17:32 -0700

    Added REPORT wikitag

---
+++ WikiTags/report.cs
@@ -0,0 +1,10 @@
+using OLabWebAPI.Common;
+using OLabWebAPI.Utils;
+
+[WikiTagModule("REPORT")]
+public class ReportWikiTag : WikiTag0Argument
+{
+    public ReportWikiTag(OLabLogger logger) : base(logger, "OlabReportTag")
+    {
+    }
+}
